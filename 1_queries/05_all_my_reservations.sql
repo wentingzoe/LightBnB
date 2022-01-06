4eaad0deb8a191b1f5f3153044013b80b103222f
wentingzoe <ywtzoe@gmail.com>
2022-01-06 17:05:14 -0500

    update getAllreservation functation in database.js and queries 05

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -2,6 +2,7 @@ SELECT reservations.*, properties.*, AVG(property_reviews.rating) FROM reservati
 JOIN properties ON properties.id = property_id
 JOIN property_reviews ON reservation_id = reservations.id
 WHERE reservations.guest_id = '1'
-GROUP BY reservations.id
+AND reservations.end_date < now()::DATE
+GROUP BY properties.id, reservations.id
 ORDER BY start_date 
 LIMIT 10;
--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,5 +1,5 @@
 const properties = require('./json/properties.json');
-// const user = require('./json/users.json');
+const users = require('./json/users.json');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -19,10 +19,8 @@ const getUserWithEmail = function(email) {
   const value = [email];
   return pool
     .query(queryString, value)
-    .then((result) => result.rows[0]? result[0]:null)
-    .catch((err) => {
-      console.log(err.message);
-    });
+    .then((result) => result.rows[0]? result.rows[0]:null)
+    .catch((err) => { console.log(err.message);});
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,10 +34,8 @@ const getUserWithId = function(id) {
   const value = [id];
   return pool
   .query(queryString, value)
-  .then((result) => result.rows[0]? result[0]:null)
-  .catch((err) => {
-    console.log(err.message);
-  });
+  .then((result) => result.rows[0]? result.rows[0]:null)
+  .catch(err => console.log(err.message));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -68,7 +64,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT reservations.*, properties.*, AVG(property_reviews.rating) FROM reservations
+  JOIN properties ON properties.id = property_id
+  JOIN property_reviews ON reservation_id = reservations.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::DATE
+  GROUP BY properties.id, reservations.id
+  ORDER BY start_date 
+  LIMIT $2;`
+  const values = [guest_id, limit]
+  return pool  
+  .query(queryString,values)
+  .then(res => res.rows)
+  .catch(err => console.log(err.message));
 }
 exports.getAllReservations = getAllReservations;
 
@@ -84,9 +92,7 @@ const getAllProperties = function(options, limit = 10) {
   return pool
   .query(`SELECT * FROM properties LIMIT $1`, [limit])
   .then((result) => result.rows)
-  .catch((err) => {
-    console.log(err.message);
-  });
+  .catch(err => console.log(err.message));
 }
 exports.getAllProperties = getAllProperties;
 
